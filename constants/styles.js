91774eca5a0c7d9da603bea04759b7fa0a197fd2
BavteqDoIT <bartek.adamek@onet.pl>
2025-05-26 22:18:53 +0200

    Changed colors

--- constants/styles.js
+++ constants/styles.js
@@ -1,7 +1,7 @@
 export const Colors = {
-  primary100: '#f9beda',
-  primary500: '#c30b64',
-  primary800: '#610440',
-  error100: '#fcdcbf',
-  error500: '#f37c13',
-}
+  primary100: '#ADD8E6',
+  primary500: '#1A2B4C',
+  primary800: '#0A1523',
+  error100: '#FFEBEE',
+  error500: '#D32F2F',
+};
--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'API_KEY_HERE';
+const API_KEY = 'AIzaSyA_tMQNn2u2dtZbw5mmZ_fAys16STMNS9c';
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
